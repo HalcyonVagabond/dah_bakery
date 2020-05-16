09af36ac49de63ee81f4ce9b1a0e1633607294aa
Jeremy Mattingly <jamattingly@live.com>
2020-05-15 23:11:29 -0500

    details iframe loading details http request in modal on list page

--- breadapp/views/bread_details.py
+++ breadapp/views/bread_details.py
@@ -17,21 +17,39 @@ def get_bread(bread_id):
             i.name AS i_name,
             i.local_source
         FROM breadapp_bread AS b
-        JOIN breadapp_breadingredient AS bi ON bi.bread_id = b.id
-        JOIN breadapp_ingredient as i ON bi.ingredient_id = i.id
+        LEFT JOIN breadapp_breadingredient AS bi ON bi.bread_id = b.id
+        LEFT JOIN breadapp_ingredient as i ON bi.ingredient_id = i.id
         WHERE b.id = ?
         """, (bread_id,))
 
-        resp = db_cursor.fetchone()
+        response = db_cursor.fetchall()
 
+        for row in response:
+            print("ROW!!!!!!")
+            print(row)            
         bread = Bread()
-        bread.id = resp['id']
-        bread.name = resp['name']
-        bread.region = resp['region']
+        bread.ingredient_list = []
+        for row in response:
+            if row == response[0]:
+                bread.id = row['id']
+                bread.name = row['name']
+                bread.region = row['region']
+                if not row['i_name'] == None:
+                    ingredient = Ingredient()
+                    ingredient.name = row['i_name']
+                    ingredient.local_source = row['local_source']
+                    bread.ingredient_list.append(ingredient)
+            else:
+                ingredient = Ingredient()
+                ingredient.name = row['i_name']
+                ingredient.amount = row['amount']
+                ingredient.local_source = row['local_source']
+                bread.ingredient_list.append(ingredient)
         
-        print(resp)
-
+        bread.length = len(bread.ingredient_list)
         return bread
+        
+        
 
 def bread_details(request, bread_id):
     if request.method == 'GET':
